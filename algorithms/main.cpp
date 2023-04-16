8de886b48e706bc1eab823d4885a467624cae540
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2023-04-16 16:48:32 +0100

    Heap sort of an array of size 10000 takes 685 microseconds, whereas merge with vector does 4460. Approximately 6.5 times faster.

--- algorithms/main.cpp
+++ algorithms/main.cpp
@@ -4,34 +4,34 @@
 using namespace std;
 using namespace std::chrono;
 
-#include "insertion-sort-vector.h"
-#include "insertion-sort-list.h"
 #include "merge-sort-vector.h"
+#include "heap-sort-array.h"
 
 vector<int> createVector () {
     return {};
 }
 
 int main(int argc, const char * argv[]) {
-    // solution for vector
-    SolutionVec solutionVec;
     vector<int> inputVec1 = createVector();
-    vector<int> inputVec2 = inputVec1;
-
-    auto start1 = high_resolution_clock::now();
-    solutionVec.insertionSortOfVector(inputVec2);
-    auto stop1 = high_resolution_clock::now();
-    auto duration1 = duration_cast<microseconds>(stop1 - start1);
-    cout << "Sorting a vector took " << duration1.count() << " microseconds with insertion sort." << endl;
+    int inputArr2[10000]; // inputVec1;
+    copy(inputVec1.begin(), inputVec1.end(), inputArr2);
+    int arrSize = sizeof(inputArr2) / sizeof(inputArr2[0]);
 
     // merge sort
     SolutionMergeVec solutionMergeVec;
-
+    auto start1 = high_resolution_clock::now();
+    solutionMergeVec.mergeSortOfVector(inputVec1);
+    auto stop1 = high_resolution_clock::now();
+    auto duration1 = duration_cast<microseconds>(stop1 - start1);
+    cout << "Sorting a vector took " << duration1.count() << " microseconds with merge sort." << endl;
+    
+    // heap sort
+    SolutionHeapSort solutionHeapSort;
     auto start2 = high_resolution_clock::now();
-    solutionMergeVec.mergeSortOfVector(inputVec2);
+    solutionHeapSort.heapSortArray(inputArr2, arrSize);
     auto stop2 = high_resolution_clock::now();
     auto duration2 = duration_cast<microseconds>(stop2 - start2);
-    cout << "Sorting a vector took " << duration2.count() << " microseconds with merge sort." << endl;
+    cout << "Sorting a vector took " << duration2.count() << " microseconds with heap sort." << endl;
 
     return 0;
 }
