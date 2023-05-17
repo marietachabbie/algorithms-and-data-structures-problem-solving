3c8c9503d873e5f6861858cdeb49db00fd3fc26c
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2023-05-17 19:05:52 +0100

    Quick sort (with a typo)

--- algorithms/qucik-sort.h
+++ algorithms/qucik-sort.h
@@ -1,12 +1,54 @@
-//
-//  qucik-sort.h
-//  algorithms
-//
-//  Created by Marieta Harutyunyan on 06.05.23.
-//
-
 #ifndef qucik_sort_h
 #define qucik_sort_h
 
+class Solution {
+public:
+    void swap(int *a, int *b) {
+      int t = *a;
+      *a = *b;
+      *b = t;
+    }
 
-#endif /* qucik_sort_h */
+    void printArray(int array[], int size) {
+      int i;
+      for (i = 0; i < size; i++)
+        cout << array[i] << " ";
+      cout << endl;
+    }
+
+    int partition(int array[], int low, int high) {
+      int pivot = array[high];
+      int i = (low - 1);
+      for (int j = low; j < high; j++) {
+        if (array[j] <= pivot) {
+          i++;
+          swap(&array[i], &array[j]);
+        }
+      }
+      swap(&array[i + 1], &array[high]);
+      return (i + 1);
+    }
+
+    void quickSort(int array[], int low, int high) {
+      if (low < high) {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+      }
+    }
+
+    void sort() {
+      int data[] = {8, 7, 6, 1, 0, 9, 2};
+      int n = sizeof(data) / sizeof(data[0]);
+      
+      cout << "Unsorted Array: \n";
+      printArray(data, n);
+      
+      quickSort(data, 0, n - 1);
+      
+      cout << "Sorted array in ascending order: \n";
+      printArray(data, n);
+    }
+};
+
+#endif
