41af1aba6d8a8fadf50cfef74e08e81384714933
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2023-05-20 18:53:17 +0100

    Return intersection of two arrays of not distinct integers

--- algorithms/intersection-of-two-arrays-ii.h
+++ algorithms/intersection-of-two-arrays-ii.h
@@ -1,12 +1,47 @@
-//
-//  intersection-of-two-arrays-ii.h
-//  algorithms
-//
-//  Created by Marieta Harutyunyan on 20.05.23.
-//
+/* The problem in leetcode:
+https://leetcode.com/problems/intersection-of-two-arrays-ii */
 
 #ifndef intersection_of_two_arrays_ii_h
 #define intersection_of_two_arrays_ii_h
 
+class Solution {
+private:
+    map<int, int> countElements(vector<int>& nums) {
+        map<int, int> counters;
+        for (auto& num : nums) {
+            if (counters.find(num) == counters.end()) {
+                counters[num] = 0;
+            }
 
-#endif /* intersection_of_two_arrays_ii_h */
+            counters[num]++;
+        }
+
+        return counters;
+    }
+
+    vector<int> compare (map<int, int>& map1, map<int, int>& map2) {
+        vector<int> result;
+        for (auto& num : map1) {
+            if (map2.find(num.first) != map2.end()) {
+                int smaller = min(map1[num.first], map2[num.first]);
+                for (int i = 0; i < smaller; i++) {
+                    result.push_back(num.first);
+                }
+            }
+        }
+
+        return result;
+    }
+
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        map<int, int> nums1Counts = countElements(nums1);
+        map<int, int> nums2Counts = countElements(nums2);
+        if (nums1Counts.size() > nums2Counts.size()) {
+            return compare(nums2Counts, nums1Counts);
+        }
+        return compare(nums1Counts, nums2Counts);
+    }
+};
+
+#endif
